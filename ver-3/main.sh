a52a38717c1b474ee602cda073f60c2533ed7dfb
Yohei Yasukawa <yasu1000@gmail.com>
2011-05-04 12:37:43 -0500

    Implement interesting tweet ranking.

--- ver-3/main.sh
+++ ver-3/main.sh
@@ -2,24 +2,24 @@
 if [ $# -eq 3 ]
 then
     printf "Getting recent tweets by %s... " $1
-    python ./simple-tag-getter/tag-getter.py http://twitter.com/$1 span class=entry-content > tweets-by-$1.txt
+    python ./simple-tag-getter/tag-getter.py http://twitter.com/$1 span class=entry-content > ./log/tweets-by-$1.txt
     printf "Done.\n"
     
     printf "Translating the tweets into English... "
-    python ./ja2en/api-translator.py tweets-by-$1.txt ja2en > tweets-by-$1_en.txt
+    python ./translator/translator.py ./log/tweets-by-$1.txt ja2en > ./log/tweets-by-$1_en.txt
     printf "Done.\n"
     
     printf "Getting %s's %s list... " $2 $3
-    python ./simple-tag-getter/tag-getter.py http://twitter.com/$2/$3 span class=entry-content > $2-$3-list.txt
+    python ./simple-tag-getter/tag-getter.py http://twitter.com/$2/$3 span class=entry-content > ./log/$2-$3-list.txt
     printf "Done.\n"
     
     printf "Translating the tweets into English... "
-    python ./ja2en/api-translator.py $2-$3-list.txt ja2en > $2-$3-list_en.txt
+    python ./translator/translator.py ./log/$2-$3-list.txt ja2en > ./log/$2-$3-list_en.txt
     printf "Done.\n"
     
     printf "Generating the interesting tweet ranking for %s... \n\n" $1
     sleep 5
-    python scoring-tweets.py tweets-by-$1_en.txt $2-$3-list_en.txt
+    python ./tweet-scorer/tweet-scorer.py ./log/tweets-by-$1_en.txt ./log/$2-$3-list_en.txt
     printf "Done.\n"
 else
     echo "Usage: main.sh TWITTER_ACCOUNT LIST_OWNER LIST_NAME"
